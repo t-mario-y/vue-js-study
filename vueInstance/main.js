e5a65ec674a48f463ddf685c50b6cd537ac8bf5a
unknown <>
2018-05-23 08:55:59 +0900

    手動mount

--- vueInstance/main.js
+++ vueInstance/main.js
@@ -1,52 +1,32 @@
-//restでうまくfetchできない。
-const app = new Vue({
-  el: "#myApp",
-  data: {
-    editFriend: null,
-    friends: [],
+//5-1.スクリプトからmountする
+const vm = new Vue({
+//  el: "#myApp",
+  data:{
+    vueFriends: [
+      {
+        first: 'Bobby',
+        last : 'Anderson',
+        age  : 25.
+      },
+      {
+        first: 'Mary',
+        last : 'Serra',
+        age  : 23,
+      },
+    ],
   },
-  methods: {
-    deleteFriend(id, i) {
-      fetch("http://rest.learncode.academy/api/vue-5/friends/" + id, {
-        method: "DELETE"
-      })
-      .then(() => {
-        this.friends.splice(i, 1);
-      })
-    },
-    updateFriend(friend) {
-      fetch("http://rest.learncode.academy/api/vue-5/friends/" + friend.id, {
-        body: JSON.stringify(friend),
-        method: "PUT",
-        headers: {
-          "Content-Type": "application/json",
-        },
-      })
-      .then(() => {
-        this.editFriend = null;
-      })
-    }
-  },
-  mounted() {
-    fetch("http://rest.learncode.academy/api/vue-5/friends")
-      .then(response => response.json())
-      .then((data) => {
-        this.friends = data;
-      })
+  mounted(){
+    console.log("now Vue Instance is mounted.");
   },
   template: `
   <div>
-    <li v-for="friend, i in friends">
-      <div v-if="editFriend === friend.id">
-        <input v-on:keyup.13="updateFriend(friend)" v-model="friend.name" />
-        <button v-on:click="updateFriend(friend)">save</button>
-      </div>
-      <div v-else>
-        <button v-on:click="editFriend = friend.id">edit</button>
-        <button v-on:click="deleteFriend(friend.id, i)">x</button>
-        {{friend.name}}
-      </div>
+    <li v-for="friend in vueFriends">
+      {{friend.first}}
     </li>
   </div>
   `,
 });
+
+// consoleで
+// vm.$mount("#myApp")
+// を実行すると、mounted()が走りマウントされる。
