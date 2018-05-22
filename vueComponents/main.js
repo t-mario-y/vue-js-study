02d7851285f3356ca0ed429f8b04899d89b59bf6
unknown <>
2018-05-23 00:40:09 +0900

    1st commit

---
+++ vueComponents/main.js
@@ -0,0 +1,45 @@
+Vue.component('friend-comp',{
+  props: ['friend'],
+  filters: {
+    fullName(val){
+      return `${val.first} ${val.last}`
+    }
+  },
+  methods: {
+     incrementAge(friend){
+       friend.age += 1;
+     },
+     decrementAge(friend){
+      friend.age -= 1;
+    }
+  },
+  template: `
+    <div>
+      <h4>{{friend|fullName}}, {{friend.age}} years old.</h4>
+      <div>
+        <span>First Name:</span><input v-model="friend.first"/>
+        <span>Last Name:</span><input v-model="friend.last"/>
+      </div>
+      <span>age:</span>
+      <button v-on:click="incrementAge(friend)">+</button>
+      <button v-on:click="decrementAge(friend)">-</button>
+    </div>
+  `
+});
+const vm = new Vue({
+  el: "#myApp",
+  data:{
+      vueFriends: [
+      {
+        first: 'Bobby',
+        last : 'Anderson',
+        age  : 25.
+      },
+      {
+        first: 'Mary',
+        last : 'Serra',
+        age  : 23,
+      },
+    ],
+  },
+})
---
+++ vueDirectives/main.js
@@ -0,0 +1,50 @@
+const vm = new Vue({
+  el: "#myApp",
+  //templateは"単一の要素"しか返せないので、divで囲みがち。
+  //"Component template should contain exactly one root element.
+  template : `
+    <div>
+      <div v-for="friend in vueFriends">
+        <h4>{{friend|fullName}}, {{friend.age}} years old.</h4>
+        <div>
+          <span>First Name:</span><input v-model="friend.first"/>
+          <span>Last Name:</span><input v-model="friend.last"/>
+        </div>
+        <span>age:</span>
+        <button v-on:click="incrementAge(friend)">+</button>
+        <button v-on:click="decrementAge(friend)">-</button>
+      </div>
+    </div>
+  `,
+  //dataプロパティには{{}}記法でアクセスできる。
+  data:{
+      vueFriends: [
+      {
+        first: 'Bobby',
+        last : 'Anderson',
+        age  : 25.
+      },
+      {
+        first: 'Mary',
+        last : 'Serra',
+        age  : 23,
+      },
+    ],
+  },
+  //filtersを掛けることでJSONデータからシンプルに記述できる
+  //computedの使い所がわからない
+  filters: {
+    fullName(val){
+      return `${val.first} ${val.last}`
+    }
+  },
+  //methodsを定義
+  methods: {
+     incrementAge(friend){
+       friend.age += 1;
+     },
+     decrementAge(friend){
+      friend.age -= 1;
+    }
+  }
+})
