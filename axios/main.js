005e0dd3b8447b0cf350db454062bfa6fa6190c0
友井 義明 <v_tomoi_y@agricom.co.jp>
2018-05-25 18:31:15 +0900

    hoge

---
+++ axios/main.js
@@ -0,0 +1,44 @@
+//WEB APIでアクセスする。
+//const fetchURI = 'https://api.github.com/repos/vueJs/vue/issues?state=open';
+//AirTableというサービスをREST APIとして動かしている。API KEYはGitHubには上げないこと。
+const fetchURI = "https://api.airtable.com/v0/appLuomWcZr2mn0fL/cdData?api_key=key5p6IkMIZ5aE4pc";
+
+const vm = new Vue({
+  el: "#myApp",
+  data:{
+    fetchDataList: [] //データの格納場所。宣言時は空
+  },
+  created : function () {
+    axios.get(fetchURI)
+      .then((response) => {
+        console.log(response);
+        this.fetchDataList = response.data.records;
+      })
+    },
+  methods : {
+    update(){
+      //Fetch APIか、axios で GET/PUT/POST/PATCH/DELETE の記法を調査します。
+    },
+    create(){
+      //TODO: Authorizeが通らない。
+      //POST https://api.airtable.com/v0/appLuomWcZr2mn0fL/cdData 401 (Unauthorized)
+      let params = new URLSearchParams();
+      params.append('headers',
+        {'Authorization': 'Bearer key5p6IkMIZ5aE4pc'}
+      );
+      params.append('data',
+        {
+          "fields": {
+            "ISBN": "B000024FDQ(NEW)",
+            "musician": "Bobby Mcferrin(NEW)",
+            "albumTitle": "Cirlcle Song(NEW)"
+          }
+        });
+
+      axios.post('https://api.airtable.com/v0/appLuomWcZr2mn0fL/cdData', params)
+        .then((response) =>{
+          console.log(resnponse);
+       })
+    }
+  },
+});
