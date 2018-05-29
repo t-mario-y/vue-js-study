64884017d417721b83cd7176e4d25f6db40ebde6
友井 義明 <v_tomoi_y@agricom.co.jp>
2018-05-29 16:52:43 +0900

    create/updateの実装

--- axios/main.js
+++ axios/main.js
@@ -1,11 +1,17 @@
 //Expressでローカルに建てたAPIサーバ越しにアクセスする。
 //データのリフレッシュはfetch()で行っているが、たぶんwatch()などでいい感じに実装できそうだ。
-const API_URI = "http://localhost:3000/samples";
+const API_URI = 'http://localhost:3000/samples';
 
 const vm = new Vue({
-  el: "#myApp",
+  el: '#myApp',
   data:{
-    recordList: [] //データの格納場所。宣言時は空
+    recordList: [], //データの格納場所。宣言時は空
+    updateRecord: { //作成/更新レコード
+      id        : '',
+      ISBN      : 'ISBN',
+      musician  : 'musician',
+      albumTitle: 'album title',
+    }
   },
   created : function () {
     this.fetch();
@@ -17,20 +23,32 @@ const vm = new Vue({
         this.recordList = response.data;
       })
     },
-    update(){
-      //Fetch APIか、axios で GET/PUT/POST/PATCH/DELETE の記法を調査します。
-    },
     create(){
       axios.post(API_URI + '/airTable/create', {
-        "ISBN"      : "B000024FDQ(client)",
-        "musician"  : "Bobby Mcferrin(client)",
-        "albumTitle": "Cirlcle Song(client)"
+        'ISBN'      : this.updateRecord.ISBN,
+        'musician'  : this.updateRecord.musician,
+        'albumTitle': this.updateRecord.albumTitle,
+      }).then((response) =>{
+        console.log(`Create succeeded in Vue.js : ${response.data}`);
+        //再表示
+        //本来であれば、予めitemオブジェクトを生成しておいてそれをajaxに投げ、
+        //APIでの更新が成功したのであればdataにitemを追加する、という方法がよさそうだ。
+        this.fetch();
       })
-        .then((response) =>{
-          console.log(`create succeeded in Vue.js : ${response.data}`);
-          //再表示
-          this.fetch();
-       })
-    }
+    },
+    update(){
+      //TODO 選択したレコードのIDをセットして送る必要がある
+      var target = this.recordList[0];
+      axios.patch(API_URI + '/airTable/update', {
+        'id'        : target.id,
+        'ISBN'      : this.updateRecord.ISBN,
+        'musician'  : this.updateRecord.musician,
+        'albumTitle': this.updateRecord.albumTitle,
+      }).then((response) =>{
+        console.log(`Update succeeded in Vue.js : ${response.data}`);
+        //再表示
+        this.fetch();
+      })
+    },
   },
 });
