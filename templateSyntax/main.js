d4ca8ed4ad38769bb4649066c74da4c36f96e7ca
Yanagiya Tatsunoshin <t.mario.yoshi@gmail.com>
2018-05-31 02:25:37 +0900

    template syntax

--- templateSyntax/main.js
+++ templateSyntax/main.js
@@ -4,14 +4,18 @@ const vm = new Vue({
     message: {
       value: 'data binding message.'
     },
-    list: ['りんご','バナナ','いちご']
+    list: ['りんご','バナナ','いちご'],
+    radius: 50, //this.radius / vm.radius で制御する
+    classState : {
+      isActive: true, //classの切り替えを this.classState.isActive を通じて制御していることに注目
+    },
   },
   created : function () {
 
   },
   methods : {
-    buttonClick(){
-      console.log('クリックされました。');
+    switchState(){
+      this.classState.isActive = !this.classState.isActive;
     }
   },
 });
