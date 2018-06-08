7123c41c6d526b552403d3abcb955c336fcbb3fc
t-mario-y(Mac) <tomoiyoshiaki@tomoiyoshiakinoMacBook-puro.local>
2018-06-09 02:05:57 +0900

    routingの追加

--- my-vue-router/src/App.vue
+++ my-vue-router/src/App.vue
@@ -1,10 +1,13 @@
 <template>
   <div id="app">
-    <img src="./assets/logo.png">
-    <router-view/>
+    <nav>
+      <router-link to="/">Home</router-link>
+      <router-link to="/product">商品情報</router-link>
+    </nav>
+    <!-- ここにパスと一致したコンポーネントが埋め込まれる -->
+    <router-view />
   </div>
 </template>
-
 <script>
 export default {
   name: 'App'
