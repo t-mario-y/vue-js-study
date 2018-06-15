9b2a3388639ae774c94a0310a5b995cfd7a929a2
t-mario-y(remote01) <v_tomoi_y@agricom.co.jp>
2018-06-15 16:17:57 +0900

    Vuetifyの学習

--- vueCliWebpack/src/main.js
+++ vueCliWebpack/src/main.js
@@ -8,8 +8,7 @@ import '../node_modules/vuetify/dist/vuetify.min.css'
 Vue.use(Vuetify)
 Vue.config.productionTip = false
 
-/* eslint-disable no-new */
-const vm = new Vue({
+new Vue({
   el: '#app',
   components: { App },
   template: '<App/>'
